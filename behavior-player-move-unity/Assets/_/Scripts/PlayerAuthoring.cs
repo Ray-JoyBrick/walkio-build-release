cc65e86a062f1eb813f392e037ce7601b3661b62
Ray Wang <ray@joybrick.cc>
2020-03-11 08:28:07 +0800

    Commit for side learning project

---
+++ behavior-player-move-unity/Assets/_/Scripts/PlayerAuthoring.cs
@@ -0,0 +1,56 @@
+﻿namespace JoyBrick.Walkio.Game
+{
+    using System;
+    using Unity.Entities;
+    using Unity.Mathematics;
+    using Unity.Rendering;
+    using Unity.Transforms;
+    using UnityEngine;
+
+    public struct Player : IComponentData
+    {
+    }
+
+    public struct PlayerMoveRange : IComponentData
+    {
+        public float Radius;
+    }
+
+    public class PlayerAuthoring : 
+        MonoBehaviour,
+        IConvertGameObjectToEntity
+    {
+        public Mesh moveRangeMesh;
+        public Material moveRangeMaterial;
+        
+        public void Convert(Entity entity, EntityManager dstManager, GameObjectConversionSystem conversionSystem)
+        {
+            dstManager.AddComponent<Player>(entity);
+            dstManager.AddComponentData<MovementRequest>(entity, new MovementRequest
+            {
+                Direction = float2.zero,
+                Strength = 0.0f
+            });
+            dstManager.AddComponentData<PlayerMoveRange>(entity, new PlayerMoveRange
+            {
+                Radius = 1.0f
+            });
+            dstManager.AddSharedComponentData<RenderMesh>(entity, new RenderMesh
+            {
+                mesh = moveRangeMesh,
+                material = moveRangeMaterial
+            });
+            dstManager.AddComponentData<RenderBounds>(entity, new RenderBounds());
+            dstManager.AddComponentData<LocalToWorld>(entity, new LocalToWorld());
+            dstManager.AddComponentData<Translation>(entity, new Translation());
+        }
+
+        private void OnDrawGizmos()
+        {
+            var p = transform.position;
+            
+            Gizmos.color = Color.cyan;
+            Gizmos.DrawWireCube(p, Vector3.one);
+        }
+    }
+}
---
+++ behavior-player-move-unity/Assets/_/Scripts/PlayerInputSystem.cs
@@ -0,0 +1,84 @@
+﻿namespace JoyBrick.Walkio.Game
+{
+    using System;
+    using Unity.Collections;
+    using Unity.Entities;
+    using Unity.Mathematics;
+    using UnityEngine;
+    using UnityEngine.InputSystem;
+
+    public struct MovementRequest : IComponentData
+    {
+        public float2 Direction;
+        public float Strength;
+    }
+
+    [DisableAutoCreation]
+    [AlwaysUpdateSystem]
+    public class PlayerInputSystem : SystemBase
+    {
+        private Input.Generated.Controls _controls;
+        private InputAction _moveAction;
+        
+        private float2 _previousMove;
+        
+        protected override void OnCreate()
+        {
+            base.OnCreate();
+            
+            _controls = new Input.Generated.Controls();
+            _moveAction = _controls.DefaultMapping.Move;
+        }
+
+        protected override void OnStartRunning()
+        {
+            base.OnStartRunning();
+            
+            _controls.Enable();
+        }
+
+        protected override void OnStopRunning()
+        {
+            base.OnStopRunning();
+            
+            _controls.Disable();
+        }
+
+        protected override void OnDestroy()
+        {
+            base.OnDestroy();
+            
+            _controls.Disable();
+        }
+
+        protected override void OnUpdate()
+        {
+            var moveInput = _moveAction.triggered ? (float2)_moveAction.ReadValue<Vector2>() : float2.zero;
+
+            // if (math.abs(moveInput.x) < 0.05f && math.abs(moveInput.y) < 0.05f)
+            // {
+            //     return;
+            // }
+
+            if (moveInput.x == _previousMove.x && moveInput.y == _previousMove.y) return;
+
+            _previousMove = moveInput;            
+            
+            Entities
+                .WithAll<Player>()
+                .WithoutBurst()
+                .ForEach((ref MovementRequest movementRequest) =>
+                {
+                    var direction = math.normalize(moveInput);
+                    var strength = math.lengthsq(moveInput);
+                    
+                    Debug.Log($"direction: {direction} strength: {strength}");
+
+                    movementRequest.Direction = direction;
+                    movementRequest.Strength = strength;
+                })
+                // .Schedule();
+                .Run();
+        }
+    }
+}
