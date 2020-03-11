f0ed99bc89e6dfa68dceb86b305196eb3ef234b8
Ray Wang <ray@joybrick.cc>
2020-03-11 18:02:26 +0800

    Add tile asset and test grid
    Loading grid at run time to simulate the simple flow

--- behaivor-unit-move-unity/Assets/_/Scripts/AssetLoadingSystem.cs
+++ behaivor-unit-move-unity/Assets/_/Scripts/AssetLoadingSystem.cs
@@ -1,9 +1,14 @@
 ﻿namespace JoyBrick.Walkio.Game
 {
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
     using UnityEngine.AddressableAssets;
     using UnityEngine.AddressableAssets.ResourceLocators;
     using UnityEngine.ResourceManagement.AsyncOperations;
 
+    // Should make this static instead of creating instance as there should actually be one singleton system in
+    // the project. This is used to be simple in the help of Extenject. Will see if the use of Extenject still makes
+    // sense at year 2020.
     public class AssetLoadingSystem :
         System.IDisposable
     {
@@ -24,6 +29,22 @@
             // At this point, addressable itself should be initialized completely
             InitializationFinished?.Invoke();
         }
+
+        public async Task<T> LoadFromAddressable<T>(string addressName) where T : UnityEngine.Object
+        {
+            var asyncOperationHandle = Addressables.LoadAssetAsync<T>(addressName);
+
+            var singleAsset = await asyncOperationHandle.Task;
+
+            return singleAsset;
+        }
+        
+        // public AsyncOperationHandle<List<AsyncOperationHandle<T>>> LoadAssetsByLabelAsync<T>(string label) where T : UnityEngine.Object
+        // {
+        //     var handle = Addressables.ResourceManager.StartOperation(
+        //         new LoadAssetsByLabelOperation(_loadedAssets, _loadingAssets, label, AssetLoadedCallback), default);
+        //     return handle;
+        // }
 
         //
         public void Dispose()
--- behaivor-unit-move-unity/Assets/_/Scripts/Bootstrap.cs
+++ behaivor-unit-move-unity/Assets/_/Scripts/Bootstrap.cs
@@ -1,9 +1,14 @@
 ﻿namespace JoyBrick.Walkio.Game
 {
+    using System.Threading.Tasks;
     using UnityEngine;
+    using UnityEngine.AddressableAssets;
+    using UnityEngine.ResourceManagement.AsyncOperations;
 
     public class Bootstrap : MonoBehaviour
     {
+        // public AssetReferenceT<GameObject> gridPrefab;
+        
         private AssetLoadingSystem _assetLoadingSystem = default;
         
         private void Start()
@@ -21,6 +26,37 @@
             Debug.Log($"Normal world systems are initialized");
             
             //
+            // var gridPrefabTask = _assetLoadingSystem.LoadFromAddressable<GameObject>("Test");
+
+            // Only wait a bit here
+            // gridPrefabTask.Wait(System.TimeSpan.FromMilliseconds(1000));
+            // Just get result directly
+            // gridPrefabTask.RunSynchronously(TaskScheduler.Current);
+            // var gridPrefab = gridPrefabTask.Result;
+            // var gridInstance = GameObject.Instantiate(gridPrefab);
+
+            Addressables.LoadAssetAsync<GameObject>("Test").Completed += OnCompleted;
+
+            
+            // // Setup ECS systems here
+            // Utility.Bootstrap.AddInitializationSystem<SpawnTeamUnitSystem>();
+            // Utility.Bootstrap.AddInitializationSystem<SpawnUnitSystem>();
+            //
+            // Utility.Bootstrap.AddSimulationSystem<PlayerInputSystem>();
+            // Utility.Bootstrap.AddSimulationSystem<DecideTargetSystem>();
+            // Utility.Bootstrap.AddSimulationSystem<AssignNewTargetToFreeUnitSystem>();
+            // Utility.Bootstrap.AddSimulationSystem<NonTeamUnitMoveSystem>();
+            //
+            // Utility.Bootstrap.AddPresentationSystem<PlayerMoveRangeSystem>();
+        }
+
+        private void OnCompleted(AsyncOperationHandle<GameObject> handle)
+        {
+            var prefab = handle.Result;
+
+            var gridInstance = GameObject.Instantiate(prefab);
+
+            // Setup ECS systems here
             Utility.Bootstrap.AddInitializationSystem<SpawnTeamUnitSystem>();
             Utility.Bootstrap.AddInitializationSystem<SpawnUnitSystem>();
             
