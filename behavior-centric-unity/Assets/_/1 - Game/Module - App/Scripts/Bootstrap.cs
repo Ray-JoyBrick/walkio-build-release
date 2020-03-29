ab3ff6e4c71d239b3c97e11f175b691929262616
Ray Wang <ray@joybrick.cc>
2020-03-29 09:29:32 +0800

    Update behavior centric project

---
+++ behavior-centric-unity/Assets/_/1 - Game/Module - App/Scripts/Bootstrap.cs
@@ -0,0 +1,23 @@
+﻿namespace JoyBrick.Walkio.Game
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+    using UniRx;
+    using UnityEngine;
+
+    public class Bootstrap : MonoBehaviour
+    {
+        private readonly CompositeDisposable _compositeDisposable = new CompositeDisposable();
+        
+        void Start()
+        {
+            
+        }
+
+        private void OnDestroy()
+        {
+            _compositeDisposable?.Dispose();
+        }
+    }
+}
