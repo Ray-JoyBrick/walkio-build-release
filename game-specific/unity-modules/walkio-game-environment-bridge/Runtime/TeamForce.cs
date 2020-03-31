cbc261c8794f92d0ed400303073e537c048552ae
Ray <ray@joybrick.cc>
2020-03-31 16:00:15 +0800

    Try to create team using authoring

---
+++ game-specific/unity-modules/walkio-game-environment-bridge/Runtime/TeamForce.cs
@@ -0,0 +1,8 @@
+namespace JoyBrick.Walkio.Game.Environment.Bridge
+{
+    using UnityEngine;
+
+    public class TeamForce : MonoBehaviour
+    {
+    }
+}
---
+++ game-specific/unity-modules/walkio-game-environment/Runtime/Authoring/TeamForceAuthoring.cs
@@ -0,0 +1,27 @@
+﻿namespace JoyBrick.Walkio.Game.Environment
+{
+    using System.Collections.Generic;
+    using Unity.Entities;
+    using UnityEngine;
+    using UnityEngine.AddressableAssets;
+    
+    using GameTemplate = JoyBrick.Walkio.Game.Template;
+
+    public class TeamForceAuthoring :
+        MonoBehaviour,
+        IConvertGameObjectToEntity
+    {
+        public int id;
+        
+        public void Convert(Entity entity, EntityManager dstManager, GameObjectConversionSystem conversionSystem)
+        {
+            // dstManager.AddComponent<RemoveAfterConversion>(entity);
+            // dstManager.AddComponentObject(entity, tileDetailAsset);
+
+            dstManager.AddComponentData<Team>(entity, new Team
+            {
+                Id = id
+            });
+        }
+    }
+}
