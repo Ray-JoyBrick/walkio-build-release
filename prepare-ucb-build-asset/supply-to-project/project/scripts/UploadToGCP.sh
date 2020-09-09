6682cb95520d31c875858b85c1dec68a40199b66
Ray Wang <ray@joybrick.cc>
2020-09-09 20:49:29 +0800

    Append parent folder to gsutil

--- prepare-ucb-build-asset/supply-to-project/project/scripts/UploadToGCP.sh
+++ prepare-ucb-build-asset/supply-to-project/project/scripts/UploadToGCP.sh
@@ -19,11 +19,11 @@ echo "Show the third one: $3"
 
 echo "${KEYFILE_STORAGE}" >> keyfile-storage.json
 
-curl -o google-cloud-sdk.tar.gz  https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-308.0.0-linux-x86_64.tar.gz
-gunzip < google-cloud-sdk.tar.gz | tar xvf -
-./google-cloud-sdk/bin/gcloud auth activate-service-account --project=walkio-271711 --key-file=keyfile-storage.json
-# ./google-cloud-sdk/bin/gsutil cp -n "some-file.zip" gs://joybrick-walkio-dev/references.zip
+# curl -o google-cloud-sdk.tar.gz  https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-308.0.0-linux-x86_64.tar.gz
+# gunzip < google-cloud-sdk.tar.gz | tar xvf -
+# ./google-cloud-sdk/bin/gcloud auth activate-service-account --project=walkio-271711 --key-file=keyfile-storage.json
+# # ./google-cloud-sdk/bin/gsutil cp -n "some-file.zip" gs://joybrick-walkio-dev/references.zip
 
 md5=$(md5sum "some-file.zip" | cut -d ' ' -f 1) \
-    && ./google-cloud-sdk/bin/gsutil cp -n "$md5.zip" "gs://${GCS_BUCKET}/addressable-assets-$md5.zip" \
+    && ./google-cloud-sdk/bin/gsutil cp "$md5.zip" "gs://${GCS_BUCKET}/addressable-assets-$md5.zip" \
     && rm "$md5.zip"
